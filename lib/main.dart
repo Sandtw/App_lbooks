750ddd912900b481330391ecfeb14b88726c6978
deadmaus2566 <i201823387@cibertec.edu.pe>
2023-01-10 15:41:22 -0500

    Para subir

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:lbooks_app/screens/welcome_screen.dart';
 
 void main() {
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -19,6 +19,94 @@ class Welcome_screen extends StatelessWidget {
                   fit: BoxFit.cover,
                 ),
               ),
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ClipOval(
+                    child: Container(
+                      child: Image.asset(
+                        'assets/icon/icon.png',
+                        width: 180,
+                        height: 180,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    'Bienvenido',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.libreBaskerville(
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text('Los mejores libros para leer gratis',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.dancingScript(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                      width: 350,
+                      height: 70,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        gradient: LinearGradient(
+                          colors: [
+                            Color.fromARGB(255, 209, 211, 47),
+                            Color.fromARGB(221, 152, 114, 10),
+                          ],
+                        ),
+                      ),
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Iniciar Sesión',
+                          style: GoogleFonts.libreBaskerville(
+                            color: Color.fromARGB(255, 19, 22, 52),
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                      width: 350,
+                      height: 70,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        gradient: LinearGradient(
+                          colors: [
+                            Color.fromARGB(255, 209, 211, 47),
+                            Color.fromARGB(221, 152, 114, 10),
+                          ],
+                        ),
+                      ),
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Registrarse',
+                          style: GoogleFonts.libreBaskerville(
+                            color: Color.fromARGB(255, 19, 22, 52),
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )),
+                ],
+              ),
             )
           ],
         ),
