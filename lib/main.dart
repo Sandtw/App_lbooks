967b73cdafeb70ff472721712cc8e1a3d362e1a8
Eliseo Falcon <eliseonop@gmail.com>
2023-01-12 21:22:05 -0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:lbooks_app/screens/screens.dart';
 import 'package:lbooks_app/providers/books_provider.dart';
+import 'package:lbooks_app/services/auth_services.dart';
+import 'package:lbooks_app/wrapper.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const AppState());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());  
 }
 
 class AppState extends StatelessWidget {
@@ -14,6 +19,7 @@ class AppState extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        Provider<AuthService>(create: (_) => AuthService()),
         ChangeNotifierProvider(
           create: (_) => BooksProvider(),
           lazy: false,
@@ -35,6 +41,7 @@ class MyApp extends StatelessWidget {
       initialRoute: SplashScreen.route,
       debugShowCheckedModeBanner: false,
       routes: {
+        '/': (context) => const Wrapper(),
         SplashScreen.route: (_) => const SplashScreen(),
         WelcomeScreen.route: (_) => const WelcomeScreen(),
         LoginScreen.route: (_) => const LoginScreen(),
